04e47adc90f269fde9d9185d320ccbd80a23598e
Brayan Romero <bromero@dondoctor.com>
2021-06-09 19:05:21 -0500

    Se agrega los datos consultados

--- app/src/main/java/com/example/juggerco/UserData.kt
+++ app/src/main/java/com/example/juggerco/UserData.kt
@@ -1,13 +1,21 @@
 package com.example.juggerco
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.analytics.FirebaseAnalytics
+import java.text.SimpleDateFormat
+import java.util.*
 
 class UserData : AppCompatActivity() {
-    private  lateinit var user: TextView
+    private  lateinit var userName: TextView
+    private  lateinit var userRH: TextView
+    private  lateinit var userDocument: TextView
+    private  lateinit var userPhone: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_data)
@@ -31,8 +39,30 @@ class UserData : AppCompatActivity() {
     private fun setup(firstName: String, secondName: String, lastName: String, secondLastName: String, RH: String, bornDate: String, documentNumber: String, documentType: String, phoneNumber: String) {
         println(firstName + secondName )
 
-        user = findViewById(R.id.textView)
-        user.text = firstName
+        var firstNameC = firstName.substring(0, 1).toUpperCase() + firstName.substring(1).toLowerCase();
+        var secondNameC = secondName.substring(0, 1).toUpperCase() + secondName.substring(1).toLowerCase();
+        var lastNameC = lastName.substring(0, 1).toUpperCase() + lastName.substring(1).toLowerCase();
+        var secondLastNameC = secondLastName.substring(0, 1).toUpperCase() + secondLastName.substring(1).toLowerCase();
+
+        userName = findViewById(R.id.textViewtName)
+        userName.text = "Nombres: $firstNameC $secondNameC $lastNameC $secondLastNameC"
+
+        userRH = findViewById(R.id.textViewRH)
+        userRH.text = "RH: $RH"
+
+        userDocument = findViewById(R.id.Document)
+        userDocument.text = "Documento: $documentType $documentNumber"
+
+        userPhone = findViewById(R.id.txtPhone)
+        userPhone.text = "Numero de celular : $phoneNumber"
+
+        val buttonReturn: Button = findViewById(R.id.btnBack)
+
+        buttonReturn.setOnClickListener {
+            val backIntent = Intent(this, Home::class.java).apply {
+            }
+            startActivity(backIntent)
+        }
 
     }
 }
